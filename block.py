0dcdcf637f6a3e8a7e90384ac6a2ca814b5fea36
Allan Miller <millerallan17@gmail.com>
2024-02-09 16:27:03 -0300

    add:  adição do bloco e função de movimento para baixo e para os lados(ainda falta verificar a logica de colição entre as paredes)

--- block.py
+++ block.py
@@ -6,16 +6,37 @@ import pygame
 class Block:
     screen_width = 500
     screen_height = 600
-    #IMAGE =  pygame.image.load(os.path.join('assets','block.png')) 
+    IMAGE =  pygame.image.load(os.path.join('assets','block.png')) 
+    VELOCIDADE = 2
 
 
-    def __init__(self,shape):
+    def __init__(self,shape,x,y):
         self.shape = shape
+        self.x = x
+        self.y = y
+        self.colision = False
 
     def draw(self,screen):
         for row in range(len(self.shape)):
             for column in range(len(self.shape[row])):
-                if self.shape[row][column] == 1:
-                    pass
-                    #screen.blit(Block.IMAGE,(Block.screen_width/2,Block.screen_height/2))
+                if self.shape[row][column] == 1: 
+                    pos_x = self.x + column * Block.IMAGE.get_width()
+                    pos_y = self.y + row * Block.IMAGE.get_height()
+                    if(pos_y + Block.IMAGE.get_height() >= Block.screen_height): # quando o bloco toca no chão
+                        self.colision = True
+
+                    screen.blit(Block.IMAGE,(pos_x,pos_y))
+
+
+    def moveDown(self):
+        self.y += Block.VELOCIDADE
+
+    def moveLeftRight(self,direction):
+        if(direction == 'right'):
+            self.x += Block.IMAGE.get_width()
+        elif(direction == 'left'):
+            self.x -= Block.IMAGE.get_width()
+
+
+
 
--- main.py
+++ main.py
@@ -26,24 +26,39 @@ block_shapes = [
 ]
 
 blockShape = [
-        [1],
-        [1],
-        [1],
+        [1,1,1],
+        [1,1,1],
+        [1,1,1],
+    ]
+blockShape2 = [
+        [1,1,1],
+        [0,1,0],
+        [0,1,0],
     ]
 
-#block = Block(blockShape)
+block = Block(blockShape,screen_width/2 - 50,0)
 
 
 running = True
 while running:
-    clock.tick(5)
+    clock.tick(100)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                block.moveLeftRight('left')
+            if event.key == pygame.K_RIGHT:
+                block.moveLeftRight('right')
      
-
-
     screen.fill((255, 255, 255))
+
+    block.draw(screen)
+    if(not block.colision):
+        block.moveDown()
+    else:
+        block = Block(blockShape2,screen_width/2 - 50,0)
+
 
     pygame.display.flip()
 
