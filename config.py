a09d5e60d702adc1050dd593ad82561a9458a6ff
Allan Miller <millerallan17@gmail.com>
2024-02-23 17:25:55 -0300

    upgrade: assets paste its now organized

--- config.py
+++ config.py
@@ -9,18 +9,18 @@ NEXT_KEY_TIME = 0
 
 BAR_COLOR = (255,255,255)
 
-IMAGE_LIST = [pygame.image.load(os.path.join("assets", "piscadinha.png")), pygame.image.load(os.path.join("assets", "bichinha.png")), pygame.image.load(os.path.join("assets", "apaixonado.png")), pygame.image.load(os.path.join("assets", "zeRuela.png")), pygame.image.load(os.path.join("assets", "medroso.png"))]
+IMAGE_LIST = [pygame.image.load(os.path.join("assets/imgs", "piscadinha.png")), pygame.image.load(os.path.join("assets/imgs", "bichinha.png")), pygame.image.load(os.path.join("assets/imgs", "apaixonado.png")), pygame.image.load(os.path.join("assets/imgs", "zeRuela.png")), pygame.image.load(os.path.join("assets/imgs", "medroso.png"))]
 SPEED = 3
-FONT_PATH = os.path.join("assets", "Honk-Regular.ttf")
+FONT_PATH = os.path.join("assets/fonts", "Honk-Regular.ttf")
 
 TICK = 30
 pygame.mixer.init()
-BACKGROUND_IMAGE = pygame.image.load(os.path.join("assets","background.png"))
-INITIAL_IMAGE = pygame.image.load(os.path.join("assets","Tetris.png"))
-GAMEOVER_IMAGE = pygame.image.load(os.path.join("assets","gameOver.png"))
-PAUSE_IMAGE = pygame.image.load(os.path.join("assets","pauseScreen.png"))
-POINT_SOUND_PATH = os.path.join("assets", "collect-ring-15982.mp3")
-GAMEOVER_SOUND_PATH = os.path.join("assets", "videogame-death-sound-43894 (1).mp3")
+BACKGROUND_IMAGE = pygame.image.load(os.path.join("assets/imgs","background.png"))
+INITIAL_IMAGE = pygame.image.load(os.path.join("assets/imgs","Tetris.png"))
+GAMEOVER_IMAGE = pygame.image.load(os.path.join("assets/imgs","gameOver.png"))
+PAUSE_IMAGE = pygame.image.load(os.path.join("assets/imgs","pauseScreen.png"))
+POINT_SOUND_PATH = os.path.join("assets/sounds", "collect-ring-15982.mp3")
+GAMEOVER_SOUND_PATH = os.path.join("assets/sounds", "videogame-death-sound-43894 (1).mp3")
 point_sound = pygame.mixer.Sound(POINT_SOUND_PATH)
 gameOver_sound = pygame.mixer.Sound(GAMEOVER_SOUND_PATH)
 
